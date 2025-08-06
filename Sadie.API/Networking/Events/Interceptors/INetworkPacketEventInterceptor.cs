e60ba9eb61d36b2eb478f40a3bc641610b1d4cb0
habtard <habtard@gmail.com>
2025-08-06 16:42:10 +0100

    updates

--- Sadie.API/Networking/Events/Interceptors/INetworkPacketEventInterceptor.cs
+++ Sadie.API/Networking/Events/Interceptors/INetworkPacketEventInterceptor.cs
@@ -1,7 +1,7 @@
 using Sadie.API.Networking.Client;
 using Sadie.API.Networking.Events.Handlers;
 
-namespace Sadie.API.Networking.Interceptors;
+namespace Sadie.API.Networking.Events.Interceptors;
 
 public interface INetworkPacketEventInterceptor<in TPacket> where TPacket : INetworkPacketEventHandler
 {
