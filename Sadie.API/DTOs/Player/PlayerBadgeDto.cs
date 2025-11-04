485a656da5a1429223b298662587055fa71d2445
habtard <habtard@gmail.com>
2025-11-04 21:02:51 +0000

    PlayerDtos

---
+++ Sadie.API/DTOs/Player/PlayerBadgeDto.cs
@@ -0,0 +1,8 @@
+namespace Sadie.API.DTOs.Player;
+
+public class PlayerBadgeDto
+{
+    public int BadgeId { get; init; }
+    public string? BadgeCode { get; init; }
+    public int Slot { get; init; }
+}
---
+++ Sadie.API/DTOs/Player/PlayerDto.cs
@@ -0,0 +1,23 @@
+namespace Sadie.API.DTOs.Player;
+
+public class PlayerDto
+{
+    public long Id { get; init; }
+    public string Username { get; init; } = string.Empty;
+    public string Email { get; init; } = string.Empty;
+    public DateTimeOffset CreatedAt { get; init; }
+
+    public PlayerDataDto? Data { get; init; }
+    public PlayerAvatarDataDto? AvatarData { get; init; }
+
+    public List<string> Roles { get; init; } = [];
+    public List<string> Tags { get; init; } = [];
+    public List<PlayerBadgeDto> Badges { get; init; } = [];
+    public List<PlayerSubscriptionDto> Subscriptions { get; init; } = [];
+
+    public PlayerNavigatorSettingsDto? NavigatorSettings { get; init; }
+    public PlayerGameSettingsDto? GameSettings { get; init; }
+
+    public int FriendshipCount { get; init; }
+    public int RoomCount { get; init; }
+}
