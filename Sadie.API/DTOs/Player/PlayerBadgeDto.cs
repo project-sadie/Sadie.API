4aabad6b395c7284d2499934398081a826a2369d
habtard <habtard@gmail.com>
2025-11-22 15:22:39 +0000

    badge stuff

--- Sadie.API/DTOs/Player/PlayerBadgeDto.cs
+++ Sadie.API/DTOs/Player/PlayerBadgeDto.cs
@@ -4,5 +4,6 @@ public record PlayerBadgeDto
 {
     public int BadgeId { get; init; }
     public string? BadgeCode { get; init; }
+    public BadgeDto? Badge { get; init; }
     public int Slot { get; init; }
 }
