e5a7d9868a2dc01e3b54db48bf5e8121e53a9a18
habtard <habtard@gmail.com>
2025-11-22 14:27:52 +0000

    updates

--- Sadie.API/DTOs/Player/PlayerAvatarDataDto.cs
+++ Sadie.API/DTOs/Player/PlayerAvatarDataDto.cs
@@ -1,9 +1,12 @@
+using Sadie.Core.Enums.Game.Players;
+using Sadie.Core.Enums.Miscellaneous;
+
 namespace Sadie.API.DTOs.Player;
 
 public record PlayerAvatarDataDto
 {
     public string FigureCode { get; init; } = string.Empty;
     public string? Motto { get; init; }
-    public string Gender { get; init; } = string.Empty;
-    public int ChatBubbleId { get; init; }
+    public PlayerAvatarGender Gender { get; init; }
+    public ChatBubble ChatBubbleId { get; init; }
 }
