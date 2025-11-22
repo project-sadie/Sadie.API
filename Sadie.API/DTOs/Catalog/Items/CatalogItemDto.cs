008ae30208bb17d361bbdc740f130c6f7c2202bd
habtard <habtard@gmail.com>
2025-11-22 14:09:53 +0000

    new dtos

---
+++ Sadie.API/DTOs/Catalog/Items/CatalogItemDto.cs
@@ -0,0 +1,18 @@
+using Sadie.API.DTOs.Furniture;
+
+namespace Sadie.API.DTOs.Catalog.Items;
+
+public class CatalogItemDto
+{
+    public int Id { get; init; }
+    public string? Name { get; init; }
+    public int CostCredits { get; init; }
+    public int CostPoints { get; init; }
+    public int CostPointsType { get; init; }
+    public ICollection<FurnitureItemDto> FurnitureItems { get; init; } = [];
+    public bool RequiresClubMembership { get; init; }
+    public string? MetaData { get; init; }
+    public int Amount { get; init; }
+    public int StackLimit { get; init; }
+    public int SellLimit { get; init; }
+}
